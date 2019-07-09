d2b2c4ee1b5d2aefe7360d3a7992460bff9d5d99
nikitadada <nikitadada@gmail.com>
2019-07-09 22:36:24 +0300

    fix

--- src/App/AdminBundle/Controller/DashboardController.php
+++ src/App/AdminBundle/Controller/DashboardController.php
@@ -2,10 +2,28 @@
 
 namespace App\AdminBundle\Controller;
 
+use App\AdminBundle\Document\Interview;
+use App\AdminBundle\Filter\InterviewFilter;
+use App\AdminBundle\Form\Interview\InterviewFilterType;
+use Symfony\Component\HttpFoundation\Request;
+
 class DashboardController extends BaseController
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('@Admin/Interview/list.html.twig');
+        $filter = new InterviewFilter();
+
+        $form = $this->createFilterForm(InterviewFilterType::class, $filter);
+        $form->handleRequest($request);
+
+        $dm = $this->container->getDocumentManager();
+        $qb = $dm->getRepository(Interview::class)->createFilteredQueryBuilder($filter);
+
+        $pagination = $this->paginate($qb);
+
+        return $this->render('@Admin/Interview/list.html.twig', [
+            'form' => $form->createView(),
+            'pagination' => $pagination,
+        ]);
     }
 }
